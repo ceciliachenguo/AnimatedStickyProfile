2e5e63dbd8a433f78d5833feb0638d48b87a57fe
CeciliaChen <cecilia@pdotf.com>
2023-08-03 13:48:55 -0400

    Initial Commit

---
+++ AnimatedStickyProfile/AnimatedStickyProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyProfileApp.swift
+//  AnimatedStickyProfile
+//
+//  Created by Cecilia Chen on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedStickyProfile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedStickyProfile
+//
+//  Created by Cecilia Chen on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
